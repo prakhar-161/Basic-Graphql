40716e2b4adefec97d36313ee8a2b215ce67e374
Prakhar Dhama <pdhama161@gmail.com>
2022-02-11 02:13:50 +0530

    mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -25,9 +25,40 @@ const resolvers = {
             return movie;
         }
     },
+
     User: {
         favouriteMovies: () => {
             return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
+        }
+    },
+
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            const lastId = UserList[UserList.length-1].id;
+            user.id = lastId + 1;
+            UserList.push(user);
+            return user;
+        },
+
+        updateUsername: (parent, args) => { 
+            // const id = args.input.ud;
+            // const newUsername = args.input.newUsername;
+            const { id, newUsername } = args.input;
+            let userUpdated;
+            UserList.forEach((user) => {
+                if (user.id === Number(id)) {
+                    user.username = newUsername;
+                    userUpdated = user;
+                }
+            });
+            return userUpdated;
+        },
+
+        deleteUser: (parent, args) => {
+            const id = args.id;
+            _.remove(UserList, (user) => user.id === Number(id));
+            return null;
         }
     }
 };
