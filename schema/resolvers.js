e51603c675c35181e7d7852a81b2f192050a912a
Prakhar Dhama <pdhama161@gmail.com>
2022-02-11 01:12:28 +0530

    movies typedef and resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,33 @@
-const { UserList } = require('../FakeData');
+const { UserList, MovieList } = require('../FakeData');
+const _ = require('lodash');
 
 const resolvers = {
     Query: {
-        users() {
+        // User resolvers
+        users: () => {
             return UserList;
+        },
+
+        user: (parent, args) => {
+            const id = args.id; 
+            const user = _.find(UserList, { id: Number(id) });
+            return user;
+        },
+
+        // Movie resolvers
+        movies: () => {
+            return MovieList;
+        },
+
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(MovieList, { name });
+            return movie;
+        }
+    },
+    User: {
+        favouriteMovies: () => {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
         }
     }
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -6,13 +6,32 @@ const typeDefs = gql`
         name: String!
         username: String!
         age: Int!
-        nationality: String!
+        nationality: Nationality!
+        friends: [User]
+        favouriteMovies: [Movie]
+    }
+
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
     }
     
     type Query {
         users: [User!]!
-        
+        user(id: ID!): User!
+        movies: [Movie!]!
+        movie(name: String!): Movie!
     }
-`
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
+    }
+`;
 
 module.exports = { typeDefs };
